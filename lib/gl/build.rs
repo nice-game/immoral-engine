16bf8e004823022bcfd8fe7f921841f420cf2515
Icy Defiance <icydefiance@gmail.com>
2020-09-13 18:18:24 -0400

    Load extension

--- lib/gl/build.rs
+++ lib/gl/build.rs
@@ -7,7 +7,7 @@ fn main() {
 	let dest = env::var("OUT_DIR").unwrap();
 	let mut file = File::create(&Path::new(&dest).join("gl_bindings.rs")).unwrap();
 
-	Registry::new(Api::Gl, (4, 5), Profile::Core, Fallbacks::All, [])
+	Registry::new(Api::Gl, (4, 3), Profile::Core, Fallbacks::All, ["GL_ARB_direct_state_access"])
 		.write_bindings(GlobalGenerator, &mut file)
 		.unwrap();
 }
