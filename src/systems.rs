d57b3fc6478ec18bc73cb5a1ed58ffdf481c6160
Icy Defiance <icydefiance@gmail.com>
2020-09-19 19:56:54 -0400

    Mouse look

--- src/systems.rs
+++ src/systems.rs
@@ -1,2 +1,3 @@
+pub mod gui;
 pub mod player;
 pub mod render;
--- src/systems/player.rs
+++ src/systems/player.rs
@@ -1,17 +1,22 @@
-use crate::PlayerController;
+use crate::{Ctx, PlayerController};
 use glutin::event::{DeviceEvent, VirtualKeyCode};
 use specs::prelude::*;
-use std::collections::VecDeque;
+use std::sync::{atomic::Ordering, Arc};
 
 pub struct PlayerSys;
 impl<'a> System<'a> for PlayerSys {
-	type SystemData = (Read<'a, VecDeque<DeviceEvent>>, WriteStorage<'a, PlayerController>);
+	type SystemData = (ReadExpect<'a, Arc<Ctx>>, Read<'a, Vec<DeviceEvent>>, WriteStorage<'a, PlayerController>);
 
-	fn run(&mut self, (events, mut players): Self::SystemData) {
+	fn run(&mut self, (ctx, events, mut players): Self::SystemData) {
+		// if !ctx.grab.load(Ordering::Relaxed) {
+		// 	return;
+		// }
+
 		let player = (&mut players).join().next().unwrap();
 
 		for event in &*events {
 			match event {
+				DeviceEvent::MouseMotion { delta: (x, y) } => player.cam.look((x * 0.01) as _, (y * 0.01) as _),
 				DeviceEvent::Key(key) => match key.virtual_keycode {
 					Some(keycode) => match keycode {
 						VirtualKeyCode::W => player.cam.uniform.pos.y += 0.1,
